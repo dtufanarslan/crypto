a96e3e2d5a6392ac9f093a3aa30f76906292c200
Esma BOSTANCI <esma.bostanci@stm.com.tr>
2025-08-29 16:24:59 +0300

    Added Primeng

---
+++ src/app/button-demo/button-demo.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { ButtonModule } from 'primeng/button';
+
+@Component({
+    selector: 'button-demo',
+    templateUrl: './button-demo.component.html',
+    standalone: true,
+    imports: [ButtonModule]
+})
+export class ButtonDemo {
+    onCheckClick() {
+        console.log('Button clicked!');
+    }
+}
