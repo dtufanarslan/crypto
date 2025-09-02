6312e4a81dbad5fd72af5df96525c054c49ed192
Deniz Tufan ARSLAN <dtufan.arslan@stm.com.tr>
2025-09-02 16:24:36 +0300

    remove example button component

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ButtonDemo } from './button-demo/button-demo.component';
 import { ExampleComponent } from './example/example.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, ButtonDemo, ExampleComponent],
+  imports: [RouterOutlet, ExampleComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
