d96cc33bfbb5fc77152d68871bcc2642107e29cd
Deniz Tufan ARSLAN <dtufan.arslan@stm.com.tr>
2025-09-04 10:40:40 +0200

    use treetable

--- src/app/example/example.component.ts
+++ src/app/example/example.component.ts
@@ -1,17 +1,29 @@
-import { Component, inject, OnInit, signal, WritableSignal } from '@angular/core';
+import { Component, computed, inject, OnInit, signal, WritableSignal } from '@angular/core';
 import { ApiService } from '../api.service';
-import { JsonPipe } from '@angular/common';
 import { TableModule } from 'primeng/table';
+import { TreeTableModule } from 'primeng/treetable';
 
 @Component({
     selector: 'example',
     templateUrl: './example.component.html',
     standalone: true,
-    imports: [TableModule, JsonPipe]
+    imports: [TableModule, TreeTableModule]
 })
 export class ExampleComponent implements OnInit {
     private api = inject(ApiService);
-    data: WritableSignal<Array<any>> = signal([]);
+    private data: WritableSignal<Array<{ id: number, name: string, data: any }>> = signal([]);
+    tableData = computed(() => {
+        let newData: Array<any> = [];
+        this.data().forEach((value) => {
+            const index = newData.findIndex((v) => v.data.name == value.name);
+            if (index !== -1) {
+                newData[index]['children'].push({ data: { id: value.id, name: JSON.stringify(value.data) } });
+            } else {
+                newData.push({ data: { id: newData.length + 1, name: value.name }, children: [ {data: { id: value.id, name: JSON.stringify(value.data) } } ] });
+            }
+        });
+        return newData;
+    });
 
     callback = {
         next: (res: Array<any>) => {
